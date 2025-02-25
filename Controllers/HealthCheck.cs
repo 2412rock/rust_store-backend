ac78d0f92fb8f5961a04fcb754db7ed6644b3bfc
Adrian Albu <2412rock@gmail.com>
2025-02-25 12:14:09 +0200

    Add healthcheck

---
+++ Controllers/HealthCheck.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rust_store_backend.Controllers
+{
+    [Route("api")]
+    public class HealthCheck: Controller
+    {
+        [HttpGet("health")]
+        public async Task<IActionResult> Health()
+        {
+            return Ok("success");
+        }
+    }
+}
